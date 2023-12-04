8a8d76b0cee6430ff520c1416cd22069f16ff061
Larry Moore <hvw@yandex.com>
2023-12-04 21:00:04 +0300

    Initial Commit

---
+++ uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikit
+//
+//  Created by Vladislav Muravyev on 04.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
